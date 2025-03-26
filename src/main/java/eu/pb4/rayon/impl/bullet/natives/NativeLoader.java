8d4a6707563e0d4e4d8b06a00f5f031a454770a5
CattySteve <cattysteve89265@163.com>
2025-03-26 11:10:23 +0800

    feat: add LoongArch support

--- src/main/java/eu/pb4/rayon/impl/bullet/natives/NativeLoader.java
+++ src/main/java/eu/pb4/rayon/impl/bullet/natives/NativeLoader.java
@@ -67,7 +67,7 @@ public class NativeLoader {
 
         final var name = switch (platform) {
             case Windows32, Windows64 -> "bulletjme.dll";
-            case Android_ARM7, Android_ARM8, Linux_ARM32, Linux_ARM64, Linux32, Linux64 -> "libbulletjme.so";
+            case Android_ARM7, Android_ARM8, Linux_ARM32, Linux_ARM64, Linux_LOONGARCH64, Linux32, Linux64 -> "libbulletjme.so";
             case MacOSX32, MacOSX64, MacOSX_ARM64 -> "libbulletjme.dylib";
             default -> throw new RuntimeException("Invalid platform " + platform);
         };
